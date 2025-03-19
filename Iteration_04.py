73109621ee4a2128db51e8b49bd1d330dd239475
malenachen <malena.chenn@gmail.com>
2025-03-18 21:21:44 -0400

    Edited case checking for add to cart feature + added to-do list at top for next iteration stage

--- Iteration_04.py
+++ Iteration_04.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+'''
+TO DO:
+    * make code not be able to recommend same game more than once
+    * put games into imported file
+    * add actual checkout feature
+    * create command to leave category and enter different game genre
+
+'''
+
 import random  
 
 # Holds string values for the code-dictonary
@@ -57,30 +66,34 @@ class GamePicker:
         # Create a variable that holds the values above. THe user will select values from above and add it to this list
 
     def pick_game(self, genre):       
-
-        selected_game = random.choice(games[genre])
-        print("========================================================")
-        print("Game: " + selected_game["title"]) 
-        # Show the game title,price, name of developer
-        print("Price: " + str(selected_game["price"])) 
-        #Has to be a str value because the team was unable to print an int value other than this fashion.
-        print("Developer: " + selected_game["developer"])
-
-        # Ask the user if they want to add the game to their cart
-        add_to_cart = input("Add this game to your cart? Type yes or no.\n").lower()
-        if add_to_cart == "yes":
-            # If the user says "yes"
-            self.cart.append(selected_game)  
-            # Add the game to the cart if no do not add to cart
-            print("Game added to your cart.") 
-   #Print game addded_to cart
-        elif add_to_cart == "no":
-            print("Game not added to your cart.")
-            return False
-        else:
-            print("Please enter a valid command.")
+        
+        keep_game = False   # boolean to keep current game selected if user types something other than provided commands.
+        
+        while True:
+            if not keep_game:   # changes selected game
+                selected_game = random.choice(games[genre])
+            keep_game = False   # boolean makes game reset next iterative process
+            print("========================================================")
+            print("Game: " + selected_game["title"]) 
+            # Show the game title,price, name of developer
+            print("Price: " + str(selected_game["price"])) 
+            #Has to be a str value because the team was unable to print an int value other than this fashion.
+            print("Developer: " + selected_game["developer"])
+    
+            # Ask the user if they want to add the game to their cart
+            add_to_cart = input("Add this game to your cart? Type yes or no.\n").lower()
+            if add_to_cart == "yes":
+                # If the user says "yes"
+                self.cart.append(selected_game)  
+                print("Game added to your cart.") #Print game addded_to cart
+                break
+            elif add_to_cart == "no":
+                print("Game not added to your cart.")   #Print game not added to cart
+            else:
+                print("Please enter a valid command.")
+                keep_game = True  # will keep current selected game when looping back to ask yes/no again
               
-#Print game not add to cart
+
    
         self.show_cart()
 
@@ -114,6 +127,7 @@ def main():
         # First check if genre is valid
         if user_input == "DONE":
             print("You have finished adding games to your cart.")   # Probably will create a more solidified checkout system later
+            picker.show_cart()
             break
         elif user_input not in GamePicker.genres:   # Check if genre is in list of genres.
             print("Please enter a valid genre.")    # If not, asks user for a valid genre.
@@ -127,3 +141,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
+#==============================================================================
+
