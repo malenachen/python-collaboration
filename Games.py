1979278cbf01ed1fd862cb336c29c3697aec16b9
malenachen <malena.chenn@gmail.com>
2025-04-03 15:46:42 -0400

    external file for all games in program

---
+++ Games.py
@@ -0,0 +1,66 @@
+# Holds string values for the code-dictonary
+games = {
+    #Holds dictonary values of the code
+    "ACTION": [
+        {"title": "Grand Theft Auto V", "price": 59.99, "developer": "Rockstar Games"},
+        {"title": "Spider-Man", "price": 49.99, "developer": "Insomniac Games"},
+        {"title": "Red Dead Redemption 2", "price": 59.99, "developer": "Rockstar Games"},
+        {"title": "Mortal Kombat 11", "price": 59.99, "developer": "NetherRealm Studios"},
+        {"title": "God of War", "price": 49.99, "developer": "Santa Monica Studio"},
+        {"title": "Dead by Daylight", "price": 7.99, "developer": "Behaviour Interactive Inc."},
+        {"title": "DOOM Eternal", "price": 59.99, "developer": "id Software"},
+        {"title": "The Outlast Trials", "price": 39.99, "developer": "Red Barrels"},
+        {"title": "ARK: Survival Evolved", "price": 14.99, "developer": "Studio Wildcard"},
+        {"title": "Destiny 2", "price": 0.00, "developer": "Bungie"},
+    ],
+    "ADVENTURE": [
+        {"title": "The Last of Us", "price": 59.99, "developer": "Naughty Dog"},
+        {"title": "Zelda: Breath of the Wild", "price": 59.99, "developer": "Nintendo"},
+        {"title": "Tomb Raider", "price": 14.99, "developer": "Crystal Dynamics"},
+        {"title": "Horizon Zero Dawn", "price": 59.99, "developer": "Guerrilla Games"},
+        {"title": "Uncharted 4", "price": 59.99, "developer": "Naughty Dog"},
+        {"title": "The Witcher 3: Wild Hunt", "price": 49.99, "developer": "CD Projekt Red"},
+        {"title": "Baldur's Gate 3", "price": 59.99, "developer": "Larian Studios"},
+        {"title": "Stray", "price": 29.99, "developer": "BlueTwelve Studio"},
+        {"title": "Celeste", "price": 19.99, "developer": "Maddy Makes Games Inc."},
+        {"title": "Hollow Knight", "price": 14.99, "developer": "Team Cherry"},
+    ],
+    "RPG": [
+        {"title": "Final Fantasy VII Remake", "price": 59.99, "developer": "Square Enix"},
+        {"title": "Persona 5", "price": 49.99, "developer": "Atlus"},
+        {"title": "The Elder Scrolls V: Skyrim", "price": 39.99, "developer": "Bethesda Game Studios"},
+        {"title": "Dark Souls III", "price": 59.99, "developer": "FromSoftware"},
+        {"title": "Cyberpunk 2077", "price": 59.99, "developer": "CD Projekt Red"},
+        {"title": "Dragon Age: Inquisition", "price": 39.99, "developer": "BioWare"},
+        {"title": "Lost Ark", "price": 0.00, "developer": "Smilegate RPG"},
+        {"title": "Elden Ring", "price": 59.99, "developer": "FromSoftware, Inc."},
+        {"title": "Assassin's Creed Odyssey", "price": 59.99, "developer": "Ubisoft"},
+        {"title": "Hades", "price": 24.99, "developer": "Supergiant Games"},
+    ],
+    "SHOOTER": [
+        {"title": "Call of Duty: Black Ops II", "price": 59.99, "developer": "Treyarch"},
+        {"title": "Rainbow 6 Siege", "price": 19.99, "developer": "Ubisoft"},
+        {"title": "Overwatch", "price": 39.99, "developer": "Blizzard Entertainment"},
+        {"title": "PUBG: BATTLEGROUNDS", "price": 0.00, "developer": "PUBG Corporation"},
+        {"title": "Battlefield V", "price": 49.99, "developer": "EA DICE"},
+        {"title": "Apex Legends", "price": 0.00, "developer": "Respawn Entertainment"},
+        {"title": "Fortnite", "price": 0.00, "developer": "Epic Games"},
+        {"title": "Valorant", "price": 0.00, "developer": "Riot Games"},
+        {"title": "Counter-Strike 2", "price": 0.00, "developer": "Valve"},
+        {"title": "Titanfall 2", "price": 29.99, "developer": "Respawn Entertainment"},
+
+    ],
+    "STRATEGY": [
+        {"title": "Civilization VI", "price": 59.99, "developer": "Firaxis Games"},
+        {"title": "Starcraft II", "price": 39.99, "developer": "Blizzard Entertainment"},
+        {"title": "Age of Empires IV", "price": 59.99, "developer": "Relic Entertainment"},
+        {"title": "Total War: Warhammer III", "price": 59.99, "developer": "Creative Assembly"},
+        {"title": "Stellaris", "price": 39.99, "developer": "Paradox Development Studio"},
+        {"title": "Dwarf Fortress", "price": 29.99, "developer": "Kitfox Games"},
+        {"title": "Bloons TD 6", "price": 13.99, "developer": " Ninja Kiwi"},
+        {"title": "Dota 2", "price": 0.00, "developer": "Valve"},
+        {"title": "Crusader Kings III", "price": 49.99, "developer": "Paradox Development Studio"},
+        {"title": "Into the Breach", "price": 14.99, "developer": "Subset Games"}
+
+    ]
+}
